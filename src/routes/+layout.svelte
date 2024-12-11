4959a7829a9df9ebab55bb1cef9d7c0512a65172
paring <paringchan@proton.me>
2024-12-11 17:12:05 +0900

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+
+	import '@adofai-gg/ui/globals.scss'
+
+	import '@fontsource/ibm-plex-sans-kr/300.css'
+	import '@fontsource/ibm-plex-sans-kr/400.css'
+	import '@fontsource/ibm-plex-sans-kr/500.css'
+	import '@fontsource/ibm-plex-sans-kr/600.css'
+	import '@fontsource/ibm-plex-sans-kr/700.css'
+
+	import { Nav, setGlobalContext } from '@adofai-gg/ui'
+	import { readable } from 'svelte/store'
+
+	interface Props {
+		children?: Snippet
+	}
+
+	const { children }: Props = $props()
+
+	setGlobalContext({
+		language: readable('en'),
+		links: [],
+		setLanguage: () => {},
+		urls: {
+			main: '/',
+			signIn: '/',
+			signUp: '/'
+		}
+	})
+</script>
+
+<Nav user={null} minimal />
+
+{@render children?.()}
